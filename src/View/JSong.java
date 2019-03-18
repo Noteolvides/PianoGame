0959a832d30f0c6e9ac88f6160b6e8806b7e657e
joseproig <josep.roig@students.salle.url.edu>
2019-03-18 11:06:00 +0100

    feat: Created the basic elements of the JSong class
    I have added the necessary attriubutes to do the view of the JSong class

--- src/View/JSong.java
+++ src/View/JSong.java
@@ -1,4 +1,28 @@
 package View;
 
+import javax.swing.*;
+
 public class JSong {
+    //Basic elements of the JSongs Class
+    //This is the View where we are going to show the songs
+    private JList songsList;
+    //This is the View that allows us to play a concrete song
+    private JButton playButton;
+
+
+    public JList getSongsList() {
+        return songsList;
+    }
+
+    public void setSongsList(JList songsList) {
+        this.songsList = songsList;
+    }
+
+    public JButton getPlayButton() {
+        return playButton;
+    }
+
+    public void setPlayButton(JButton playButton) {
+        this.playButton = playButton;
+    }
 }

0b6897c0bdc73e4ed0a415719bcf5ce55675c269
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-03-18 10:51:57 +0100

    feat : Added first Components

--- src/Client/View/JPiano.java
+++ src/Client/View/JPiano.java
@@ -4,7 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JPiano extends JFrame {
+    private JPanel topOption;
+    private JPanel botOption;
+    private JPanel leftOption;
+    private JPanel rightOption;
+    private ViewPiano piano;
     public JPiano() {
         super("Piano");
+        setLayout(new BorderLayout());
+        
     }
 }

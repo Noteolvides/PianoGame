0a5a61922e2ca1d708461a272045deaabc9b491c
Jiahu <Jiahui1@hotmail.es>
2019-03-18 16:46:32 +0100

    feat: Window created Right now JTop is implemented inside a JFrame, however this needs to be removed, cause it's a JPanel.

--- src/Server/View/JTop.java
+++ src/Server/View/JTop.java
@@ -1,16 +1,42 @@
 package Server.View;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class JTop {
     public JList songTop;
 
-    void jTop(JList songTop){
-        this.songTop = songTop;
+    void JTop(){
+        ArrayList<String> topList = new ArrayList<>();
+        topList.add("Watanabe");
+        topList.add("Mayu");
+        topList.add("is");
+        topList.add("love");
+        topList.add("!");
 
+        Border border = BorderFactory.createLineBorder(Color.GREEN);
+        this.songTop = new JList(topList.toArray());
 
+        JPanel panelTop = new JPanel();
+        panelTop.add(songTop);
+
+        panelTop.setBorder(border);
+        JFrame frameTop = new JFrame("TOP 5 - Popular Songs"); //Prova
+
+        frameTop.setSize(325, 500);
+        frameTop.setVisible(true);
+        frameTop.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameTop.add(panelTop);
     }
-    void registerController(Controller controller){
 
+    public static void main(String[] args) {
+        JTop h = new JTop();
+        h.JTop();
     }
+
+    /*void registerController(Controller controller){
+
+    }*/
 }

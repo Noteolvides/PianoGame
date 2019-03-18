37567e229210e1f43da5b544308c1581c900fbcd
Jiahu <Jiahui1@hotmail.es>
2019-03-18 11:30:48 +0100

    feat: Classe JEvolution creada First commit de la UI de JEvolution que és la gràfica de l'evolució

---
+++ src/Server/View/JEvolution.java
@@ -0,0 +1,14 @@
+package Server.View;
+
+public class JEvolution {
+    public class JEvolution {
+        void JEvolution(){
+
+
+        }
+
+        void registerController(ontroller controller){
+
+        }
+    }
+}

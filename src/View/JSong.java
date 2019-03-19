905642c33c25fe8832981e6a7bf391f4ba0072da
joseproig <josep.roig@students.salle.url.edu>
2019-03-19 21:04:41 +0100

    feat:Added new attributes to the class with the implementation of the constructor
    I have added a new element to show the title of the panel, and I have done
    the implementation of the constructor (with the basic initialitations too).

--- src/View/JSong.java
+++ src/View/JSong.java
@@ -2,13 +2,36 @@ package View;
 
 import javax.swing.*;
 
-public class JSong {
+public class JSong extends JPanel{
     //Basic elements of the JSongs Class
     //This is the View where we are going to show the songs
     private JList songsList;
     //This is the View that allows us to play a concrete song
     private JButton playButton;
+    private JButton backButton;
+    //This is the text element where we are going to show the different songs
+    private JTextArea titlePanel;
 
+    public JSong () {
+        //Inicialization of Layout
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+
+        //Inicialitation of the visual elements
+        songsList = new JList ();
+        playButton = new JButton ();
+        backButton = new JButton ();
+        titlePanel = new JTextArea();
+
+        //We put all this elements in the JPanel
+        add(songsList);
+        add(playButton);
+        add(backButton);
+
+    }
+
+    public void makePanelVisible () {
+        setVisible(true);
+    }
 
     public JList getSongsList() {
         return songsList;
@@ -24,5 +47,12 @@ public class JSong {
 
     public void setPlayButton(JButton playButton) {
         this.playButton = playButton;
+    }
+
+    public JButton getBackButton() {
+        return backButton;
+    }
+    public void setBackButton(JButton backButton) {
+        this.backButton = backButton;
     }
 }

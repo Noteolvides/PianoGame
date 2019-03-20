918ace59fa08e892efa4f32daef35d73fa2d85f8
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-03-20 10:55:02 +0100

    feat: Added decoration at Sign In
    Added decoration items and refined UI of the sign in start window.

--- src/Client/View/JStart.java
+++ src/Client/View/JStart.java
@@ -1,12 +1,17 @@
 package Client.View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 public class JStart extends JPanel {
     public static void main(String[] args) {
         JFrame test = new JFrame();
         test.getContentPane().add(new JStart());
-        test.setSize(200, 200);
+        test.setSize(300, 300);
         test.setTitle("JStart Test");
         test.setLocationRelativeTo(null);
         test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,19 +24,45 @@ public class JStart extends JPanel {
     private JButton buttonRegister;
 
     public JStart (){
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        JLabel emailLabel = new JLabel("Input your UserName or Email:");
-        add(emailLabel);
-        emailLogin = new JTextField();
-        add(emailLogin);
-        JLabel passwordLabel = new JLabel("Input your password:");
-        add(passwordLabel);
-        passwordField = new JPasswordField();
-        add(passwordField);
-        buttonSignIn = new JButton("Sign In");
-        add(buttonSignIn);
-        buttonRegister = new JButton("Register");
-        add(buttonRegister);
-    }
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.PAGE_AXIS));
+        JLabel title = new JLabel("SMART PIANO");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+        title.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+        titlePanel.add(title);
+        add(titlePanel);
 
+        JPanel fields = new JPanel();
+        fields.setLayout(new BoxLayout(fields, BoxLayout.PAGE_AXIS));
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new URL(
+                    "https://tse2.mm.bing.net/th?id=OIP.AalzUDPdSRN0Oo5wZZKgngHaCl&pid=Api&P=0&w=300&h=300"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ImageIcon icon = new ImageIcon(img);
+        JLabel imagen = new JLabel();
+        imagen.setIcon(icon);
+        fields.add(imagen);
+
+        JLabel emailLabel = new JLabel("Input your UserName or Email:");
+        fields.add(emailLabel);
+        emailLogin = new JTextField();
+        fields.add(emailLogin);
+        JLabel passwordLabel = new JLabel("Input your password:");
+        fields.add(passwordLabel);
+        passwordField = new JPasswordField();
+        fields.add(passwordField);
+        add(fields);
+
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new FlowLayout());
+        buttonSignIn = new JButton("Sign In");
+        buttons.add(buttonSignIn);
+        buttonRegister = new JButton("Register");
+        buttons.add(buttonRegister);
+        add(buttons);
+    }
 }

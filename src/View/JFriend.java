6687188c789db22a77597be3d94b98112c65ae40
joseproig <josep.roig@students.salle.url.edu>
2019-03-20 19:45:27 +0100

    feat: Created the primary attributes of the JSocial class
    I have created the necessary attributes to start developing the JSocial class.

---
+++ src/View/JFriend.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+
+public class JFriend extends JPanel {
+    //In this attribute we are going to show the photo of the friend that we find
+    private JLabel profileImageFriend;
+    //In this view we are going to show the different characteristics of the user
+    private JPanel groupCharacteristics;
+    //With this button we can add this friend or not
+    private JLabel buttonAddFriend;
+}
--- src/View/JSocial.java
+++ src/View/JSocial.java
@@ -1,5 +1,15 @@
 package View;
 
-public class JSocial {
+import javax.swing.*;
+
+public class JSocial extends JPanel {
+    //This is the square t¡where we would search someone by her code
+    private JTextField squareSeachFriend;
+    //This is attribute would be visible when the user fins someone or when you don't find anyone (it would appear a mesaage)
+    private JPanel panelFriend;
+    //This is the button to search new friends
+    private JLabel searchButton;
+    //This is the button to go back to another frame
+    private JLabel backButton;
 
 }

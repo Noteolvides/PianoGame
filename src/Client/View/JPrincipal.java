13615cf57845a1b1e9d44a85534a8b87cf26b19c
Melgui <gmelgares9@gmail.com>
2019-03-20 18:08:25 +0100

    feat: Added basics buttons and future functions Class JPrincipal
    Afegits primers botons que ens ajudaran a accedir als diferents elements
    del programa

--- src/Client/View/JPrincipal.java
+++ src/Client/View/JPrincipal.java
@@ -6,17 +6,16 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 public class JPrincipal extends JFrame {
     private BufferedImage iTitle;
 
     private JLabel jGameTitle;
 
-    private JButton jPiano;
-    private JButton jSocial;
-    private JButton jSignOut;
-    private JButton jNoMeAcuerdo;
+    private JButton buttonPiano;
+    private JButton buttonAmics;
+    private JButton buttonSignOut;
+    private JButton buttonDeleteAccount;
 
     /**
      * Constructor encarregat de mostrar la finestra principal deicada a l'accés a les funcionalitats principals
@@ -35,12 +34,33 @@ public class JPrincipal extends JFrame {
             ex.printStackTrace();
         }
 
+        JPanel vButtons = new JPanel();
+        vButtons.setLayout(new BoxLayout(vButtons, BoxLayout.Y_AXIS));
+
+        buttonPiano = new JButton("Piano");
+        vButtons.add(buttonPiano);
+        buttonAmics = new JButton("Social");
+        vButtons.add(buttonAmics);
+
+        JPanel vOut = new JPanel();
+        vOut.setLayout(new FlowLayout());
+
+        buttonSignOut = new JButton("Sign Out");
+        vOut.add(buttonSignOut);
+        buttonDeleteAccount = new JButton("Delete Account");
+        vOut.add(buttonDeleteAccount);
+
+
+        this.getContentPane().add(vButtons, BorderLayout.CENTER);
+        this.getContentPane().add(vOut, BorderLayout.PAGE_END);
+        pack();
+
         //Definim propietats de la finestra
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(600,600);
+        //setSize(600,600);
         //setMinimumSize(getSize());
-        setTitle("LSGym");
+        setTitle("Smart Piano");
         setVisible(true);
     }
 }

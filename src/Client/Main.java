1657ea2fe18163f38d7d7384f3d2193ca8d5f85f
Melgui <gmelgares9@gmail.com>
2019-04-01 23:43:00 +0200

    feat: Edited main

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,7 +1,12 @@
 package Client;
 
+import Client.View.JGestor;
+import Client.View.JSong;
+import Client.View.Prueba;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Prueba controller = new Prueba();
+        JGestor view = new JGestor(controller.simulationOfController());
     }
 }

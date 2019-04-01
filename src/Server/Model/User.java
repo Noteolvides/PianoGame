99ef3dab1fb306e9b6ce2a879f83db242b5e86bb
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-01 17:43:28 +0200

    feat: Added model Class User
    Added class User to the model.

---
+++ src/Server/Model/User.java
@@ -0,0 +1,47 @@
+package Server.Model;
+
+public class User {
+    private String name;
+    private String url;
+    private String password;
+    private String email;
+
+    public User(String name, String url, String password, String email) {
+        this.name = name;
+        this.url = url;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

a99a6e9356b8fa658618015023ac265d25295870
Melgui <gmelgares9@gmail.com>
2019-04-02 10:40:56 +0200

    feat: Added new attribute for SongPrueba. Added the constructors and getters&setters

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,12 +1,11 @@
 package Client;
 
 import Client.View.JGestor;
-import Client.View.JSong;
 import Client.View.Prueba;
 
 public class Main {
     public static void main(String[] args) {
         Prueba controller = new Prueba();
-        JGestor view = new JGestor(controller.simulationOfController());
+
     }
 }

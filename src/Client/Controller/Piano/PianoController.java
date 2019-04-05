4c77b14ba023d8ccf989b92ffec20a994d9e379e
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-04-05 17:36:55 +0200

    feat : Inialization of piano controller

---
+++ src/Client/Controller/Piano/PianoController.java
@@ -0,0 +1,18 @@
+package Client.Controller.Piano;
+
+import javax.swing.text.View;
+
+public class PianoController {
+
+    public static void main(String[] args) {
+
+    }
+
+    public PianoController() {
+        initController();
+    }
+
+    private void initController() {
+
+    }
+}

be933b4a162ac4b53197d7e6273e527f727b56ab
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-06 20:40:11 +0200

    #13 feat: Added constructor and rought actionPerformed
    Added constructor to associate with view and a rought actionpermformed action for the buttons in JStart view.

--- src/Client/Controller/Start/StartController.java
+++ src/Client/Controller/Start/StartController.java
@@ -1,16 +1,30 @@
 package Client.Controller.Start;
 
+import Client.View.Start.JRegister;
+import Client.View.Start.JStart;
+import Client.View.View;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class StartController implements ActionListener {
-
-    public StartController() {
-
+    private View view;
+    public StartController(View view) {
+        this.view = view;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getActionCommand().equals("SIGN-IN")){
+            System.out.println("Sing in");
+        }
+        if (e.getActionCommand().equals("GO-REGISTER")){
+            System.out.println("Register");
+            view.initRegisterView();
+            view.getRegisterView().registerController(this);
+        }
+        if (e.getActionCommand().equals("REGISTER")) {
+            System.out.println("Registered");
+        }
     }
 }

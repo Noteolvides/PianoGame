3ad2d302558967c2dfe3ff2a7e4cce51abc5b23b
joseproig <josep.roig@students.salle.url.edu>
2019-04-06 16:45:03 +0200

    #16 feat: Prepared the main JSocial window I have defined the attributes and the different methods of get / set

--- src/Client/View/FinestraJSocial.java
+++ src/Client/View/FinestraJSocial.java
@@ -2,11 +2,20 @@ package Client.View;
 import javax.swing.*;
 
 public class FinestraJSocial extends JFrame {
+    private JSocial jSocial;
     public FinestraJSocial () {
+        jSocial = new JSocial();
         JFrame test = new JFrame();
         test.setSize(400,405);
-        test.add(new JSocial());
+        test.add(jSocial);
         test.setVisible(true);
         test.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public JSocial getjSocial() {
+        return jSocial;
+    }
+
+    public void setjSocial(JSocial jSocial) {
+        this.jSocial = jSocial;
     }
 }

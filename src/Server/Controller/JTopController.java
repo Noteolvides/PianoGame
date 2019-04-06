6c68ccc1d0c827b45dc500ac140d28c4c4b2cc62
MasterofDragons <Jiahui1@hotmail.es>
2019-04-06 10:28:06 +0200

    #17 Feat: JTopController Action Actions added to the JTopController

--- src/Server/Controller/JTopController.java
+++ src/Server/Controller/JTopController.java
@@ -1,4 +1,39 @@
 package Server.Controller;
 
-public class JTopController {
+import Server.View.JTop;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class JTopController implements MouseListener {
+    private JTop jTop;
+
+
+    public JTopController(JTop jTop) {
+        this.jTop = jTop;
+    }
+
+    public void mousePressed(MouseEvent event){
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event){
+        System.out.println("Clicked");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }

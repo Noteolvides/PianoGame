70837fde14056832347e1d7d19cffa568aa2264f
MasterofDragons <Jiahui1@hotmail.es>
2019-04-06 11:00:42 +0200

    #17 Feat: Functions Each image mapped with a controller and assigned to the corresponded function

--- src/Server/Controller/JTopController.java
+++ src/Server/Controller/JTopController.java
@@ -1,7 +1,10 @@
 package Server.Controller;
 
 import Server.View.JTop;
+import Server.View.SongPrueba;
+import Server.View.SongView;
 
+import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -19,7 +22,22 @@ public class JTopController implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent event){
-        System.out.println("Clicked");
+        JLabel whichButton = (JLabel) event.getSource();
+        for(int i = 0; i < jTop.getSongsList().size(); i++){
+            if(jTop.getSongsList().get(i).getPlayButton().equals(whichButton)){
+                System.out.println("Playing: " + jTop.getSongsList().get(i).getTitleSong().getText());
+            }
+        }
+        for(int i = 0; i < jTop.getSongsList().size(); i++){
+            if(jTop.getSongsList().get(i).getMusicIcon().equals(whichButton)){
+                System.out.println(jTop.getSongsList().get(i).getTitleSong().getText() + ":   " + jTop.getSongsList().get(i).getDescription().getText());
+            }
+        }
+        for(int i = 0; i < jTop.getSongsList().size(); i++){
+            if(jTop.getSongsList().get(i).getInfoIcon().equals(whichButton)){
+                System.out.println(jTop.getSongsList().get(i).getDescription().getText());
+            }
+        }
     }
 
     @Override

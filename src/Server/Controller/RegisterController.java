23cc288f8f78b1ec2465540a892b17e5cfbf7448
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-07 13:10:29 +0200

    #11 Added rought controller
    Added a rought controller for the register in the server.

---
+++ src/Server/Controller/RegisterController.java
@@ -0,0 +1,19 @@
+package Server.Controller;
+
+import Server.View.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegisterController implements ActionListener {
+    private View view;
+
+    public RegisterController(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}

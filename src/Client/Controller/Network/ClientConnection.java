3500ab14cde4ee3d6293d8945547ba8241f6b48b
Melgui <gmelgares9@gmail.com>
2019-04-14 16:33:31 +0200

    #22 feat: Created establishConnection.
    This function enables the path that connects the client with the server

--- src/Client/Controller/Network/ClientConnection.java
+++ src/Client/Controller/Network/ClientConnection.java
@@ -1,5 +1,8 @@
 package Client.Controller.Network;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientConnection {
     //Connection const.
     private static final int PORT = 5000;
@@ -15,4 +18,25 @@ public class ClientConnection {
      */
     public ClientConnection() {
 
+    }
+
+    /**
+     * As a user, we make a request to the server to establish connection
+     */
+    public void establishConnection() {
+        try{
+            server = new Socket(IP, PORT);
+            System.out.println("[CLIENT] Connection established");
+
+            //Todo, establir camins de connexions
+
+        } catch (IOException e) {
+            System.out.println("Error to set up connection with the server.");
+            e.printStackTrace();
+        }
+
+    }
 }
+
+
+

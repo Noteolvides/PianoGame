4560b5ff4a1afd3705b3acbede20c52340108a33
Melgui <gmelgares9@gmail.com>
2019-04-14 16:32:36 +0200

    #22 feat: Created ClientConnection constructor and added different attributes

--- src/Client/Controller/Network/ClientConnection.java
+++ src/Client/Controller/Network/ClientConnection.java
@@ -1,5 +1,18 @@
 package Client.Controller.Network;
 
 public class ClientConnection {
+    //Connection const.
+    private static final int PORT = 5000;
+    private static final String IP = "localhost";
+
+    //Controller
+
+    //Connection elements
+    Socket server;
+
+    /**
+     * Client sockets controller
+     */
+    public ClientConnection() {
 
 }

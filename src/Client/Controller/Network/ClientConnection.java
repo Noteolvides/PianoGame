ac728eed731407bfaefdf8f392d8d82be8853347
Melgui <gmelgares9@gmail.com>
2019-04-15 12:39:29 +0200

    #22 feat: Added different constants for the communication between server & client activities.

--- src/Client/Controller/Network/ClientConnection.java
+++ src/Client/Controller/Network/ClientConnection.java
@@ -13,6 +13,19 @@ public class ClientConnection {
     private static final int RECEIVED = 0;
     private static final int ERROR = -1;
 
+    private static final int LOGIN = 1;
+    private static final float SEND_LOG_USER = 1.1f;
+    private static final float GO_BACK = -1.1f;
+
+    private static final int REGISTER = 2;
+    private static final float SEND_REG_USER = 2.1f;
+
+    private static final int PIANO = 3;
+
+    private static final int SOCIAL = 4;
+    private static final float SEARCH_USER = 4.1f;
+    private static final float ADD_USER = 4.2f;
+
     //Controller
     private Controller controller;
 

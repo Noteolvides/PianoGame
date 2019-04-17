686efcfc00d8b688a71b8db419ae25b7592dd172
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-04-17 17:19:27 +0200

    #23 Feat : Added principal logic to DedicatedServer

--- src/Server/Network/DedicatedServer.java
+++ src/Server/Network/DedicatedServer.java
@@ -1,13 +1,42 @@
 package Server.Network;
 
+import java.io.*;
 import java.net.Socket;
 
 public class DedicatedServer extends Thread{
+    private Socket socket;
+    private Server server;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+    private boolean running;
 
+    public DedicatedServer(Socket socket, Server server) throws IOException {
+        this.socket = socket;
+        this.server = server;
+        startDedicatedServer();
+    }
 
-    public DedicatedServer(Socket socket, Server server) {
+    private void startDedicatedServer() throws IOException {
+        objectInputStream = new ObjectInputStream(socket.getInputStream());
+        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        
+        running = true;
+        start();
     }
 
     public void stopDedicatedServer() {
+        running = false;
+        this.interrupt();
+    }
+
+    @Override
+    public void run() {
+        while (running && !isInterrupted()){
+
+        }
     }
 }

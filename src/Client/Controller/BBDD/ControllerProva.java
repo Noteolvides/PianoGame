3bff25a56f1d0b59cddc3db317480fddfefac4cb
joseproig <josep.roig@students.salle.url.edu>
2019-04-21 00:05:48 +0200

    #21 feat: Created the XML file to load the Spring configuration
    I have uploaded the configuration file that will allow us to inject directly the different attributes that the connection with the database will need. For this reason, some basic attributes have also been created in each class too.

--- src/Client/Controller/BBDD/ControllerProva.java
+++ src/Client/Controller/BBDD/ControllerProva.java
@@ -1,4 +1,18 @@
 package Client.Controller.BBDD;
 
+import Client.Controller.BBDD.ServiceBBDD.ServiceBBDDUser;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ControllerProva {
+    @Autowired
+    private ServiceBBDDUser service;
+
+
+    public ServiceBBDDUser getService() {
+        return service;
+    }
+
+    public void setService(ServiceBBDDUser service) {
+        this.service = service;
+    }
 }
--- src/Client/Controller/BBDD/ServiceBBDD/ServiceBBDDUser.java
+++ src/Client/Controller/BBDD/ServiceBBDD/ServiceBBDDUser.java
@@ -1,4 +1,18 @@
 package Client.Controller.BBDD.ServiceBBDD;
 
+import Client.Controller.BBDD.DAOBBDD.DAOUser;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ServiceBBDDUser {
+    @Autowired
+    private DAOUser dao;
+
+
+    public DAOUser getDao() {
+        return dao;
+    }
+
+    public void setDao(DAOUser dao) {
+        this.dao = dao;
+    }
 }

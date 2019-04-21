a7d46c5112c88d0c2328a35dad1d75fa080b72a0
joseproig <josep.roig@students.salle.url.edu>
2019-04-21 02:24:59 +0200

    #21 feat: Created the method to check the existence of a User in mysql server
    NOw, we can check if tje user exists in mysql server.

--- src/Client/Controller/BBDD/DAOBBDD/DAOUser.java
+++ src/Client/Controller/BBDD/DAOBBDD/DAOUser.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Query;
+import java.util.List;
 
 @Repository
 public class DAOUser extends HibernateDaoSupport {
@@ -22,26 +23,40 @@ public class DAOUser extends HibernateDaoSupport {
         super.setSessionFactory(sessionFactory);
     }
 
-
+    //TODO: Conexion diferente
     //This method is responsible for checking the existence of a specific user within the database
     @Transactional
-    public boolean checkExistenceUser (String username, String password) {
+    public boolean checkExistenceUserDatabase (String username, String password) {
         String query = "SELECT COUNT(*) FROM User AS u WHERE u.name =? AND u.password =?";
         Object [] queryParameters = {username,password};
+
+        List<?> num = getHibernateTemplate().find (query, queryParameters);
+        //TODO: Comprobar resultado
+        if (num.get(0).equals("0")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    //With this method we can now check the existence of the user in our mysql server
+    @Transactional
+    public boolean checkExistenceUserMysql (String username, String password) {
         try {
-            int num = getHibernateTemplate().find (query, queryParameters);
-            if (num == 0) {
-                return false;
-            }
-            else {
-                return true;
-            }
+            boolean result = (boolean) getHibernateTemplate().execute(new HibernateCallback<Object>() {
+                public Object doInHibernate(Session session) throws HibernateException {
+                    Query query = session.createSQLQuery("SELECT COUNT(*) FROM mysql.user WHERE user = " + username);
+                    int i = query.executeUpdate();
+                    return i != 0;
+                }
+            });
+            return result;
         } catch (Throwable e) {
             e.printStackTrace();
             return false;
         }
     }
-
 
 
 

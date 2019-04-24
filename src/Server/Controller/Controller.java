4c4596e75bf4a0eb5bdb81e7f51c80b7886d20b5
Jiahu <Jiahui1@hotmail.es>
2019-04-24 15:31:04 +0100

    #19 Added: New Controller Remake of the old Grand Controller

---
+++ src/Server/Controller/Controller.java
@@ -0,0 +1,23 @@
+package Server.Controller;
+
+import Server.View.JEvolution;
+import Server.View.JTop;
+import Server.View.View;
+
+public class Controller {
+    private JEvolutionController jEvolutionController;
+    private JTopController jTopController;
+    private RegisterController registerController;
+
+    public Controller(JEvolutionController jEvolutionController, JTopController jTopController, RegisterController registerController){
+        this.jEvolutionController = jEvolutionController;
+        this.jTopController = jTopController;
+        this.registerController = registerController;
+    }
+
+    public void actionManager(JEvolution jEvolution, JTop jTop, View view){
+        jEvolutionController = new JEvolutionController(jEvolution);
+        jTopController = new JTopController(jTop);
+        registerController = new RegisterController(view);
+    }
+}

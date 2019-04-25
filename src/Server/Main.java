1fd62c280325fde4c4aeefc2d6f6ece9925b3d6b
Jiahu <Jiahui1@hotmail.es>
2019-04-25 16:40:50 +0100

    #19 Added: Server view Server view for every panel that server has.

--- src/Server/Main.java
+++ src/Server/Main.java
@@ -8,11 +8,35 @@ import Server.View.JEvolution;
 import Server.View.JTop;
 import Server.View.View;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        View view = new View();
+        JFrame serverFrame = new JFrame("Server Functionalities");
+        serverFrame.setLayout(new GridLayout(3, 1));
+
+        JButton serverGraph, topSongs, registerUser;
+        registerUser = new JButton("Register User");
+        ImageIcon iconRegister = new ImageIcon("img/registerIcon.png");
+        registerUser.setIcon(resizeIcon(iconRegister, iconRegister.getIconWidth()/8, iconRegister.getIconHeight()/8));
+        topSongs = new JButton("Top 5 Songs");
+        ImageIcon iconTop = new ImageIcon("img/topicon.png");
+        topSongs.setIcon(resizeIcon(iconTop, iconTop.getIconWidth()/7, iconTop.getIconHeight()/7));
+        serverGraph = new JButton("User Graph");
+        ImageIcon iconGraph = new ImageIcon("img/graphicicon.png");
+        serverGraph.setIcon(resizeIcon(iconGraph, iconGraph.getIconWidth()/35, iconGraph.getIconHeight()/35));
+
+        serverFrame.add(registerUser);
+        serverFrame.add(topSongs);
+        serverFrame.add(serverGraph);
+
+        serverFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        serverFrame.setSize(300, 350);
+        serverFrame.setVisible(true);
+
+        /*View view = new View();
         view.initRegisterView();
         RegisterController controller = new RegisterController(view);
         //view.getRegisterView().registerController(controller);
@@ -25,6 +49,11 @@ public class Main {
         JTopController topController = new JTopController(top);
 
         Controller control = new Controller(evoController, topController, controller);
-        control.actionManager(evolution, top, view);
+        control.actionManager(evolution, top, view);*/
+    }
+    private static Icon resizeIcon(ImageIcon icon, int resizedWidth, int resizedHeight) {
+        Image img = icon.getImage();
+        Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight,  java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(resizedImage);
     }
 }

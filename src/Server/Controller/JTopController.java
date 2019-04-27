e914e6a241b812d4b3c24e2cdd5c0733aaef96fa
Jiahu <Jiahui1@hotmail.es>
2019-04-27 14:53:46 +0100

    #25 Feat: Stops Music It stops the song if we click the Stop button

--- src/Server/Controller/JTopController.java
+++ src/Server/Controller/JTopController.java
@@ -7,12 +7,13 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class JTopController implements MouseListener {
     private JTop jTop;
     private Set<Thread> setOfThread;
-    private playSong music;
+    private ArrayList<playSong> music = new ArrayList<>();
 
     public JTopController(JTop jTop) {
         this.jTop = jTop;
@@ -40,13 +41,14 @@ public class JTopController implements MouseListener {
 
                 //TODO: Add Thread to Play Music
                 jTop.getSongsList().get(i).setPlaying(true);
-                music = new playSong(songTitle);
-                Thread playMusic = new Thread(music, jTop.getSongsList().get(i).getTitleSong().getText());
-                playMusic.start();
+                music.add(new playSong(songTitle));
+                //Thread playMusic = new Thread(music, jTop.getSongsList().get(i).getTitleSong().getText());
+                //playMusic.start();
+                music.get(music.size()-1).start();
 
-
+                //System.out.println("Thread created: " + playMusic.getName() +  "   " + playMusic.isAlive());
                 setOfThread = Thread.getAllStackTraces().keySet();
-                System.out.println("Thread created: " + playMusic.getName() +  "   " + playMusic.isAlive());
+                //System.out.println("Thread created: " + music.getName() +  "   " + music.isAlive());
             }else{
                 if (jTop.getSongsList().get(i).getMusicIcon().equals(whichButton) && jTop.getSongsList().get(i).isPlaying()){
                     jTop.getSongsList().get(i).setPlaying(false);
@@ -54,12 +56,18 @@ public class JTopController implements MouseListener {
                     jTop.getSongsList().get(i).resetMusicIcon(i);
 
                     //Iterate over set to find yours
-                    for(Thread thread : setOfThread){
+                    /*for(Thread thread : setOfThread){
                         if(thread.getName().equals(jTop.getSongsList().get(i).getTitleSong().getText())){
-                            System.out.println(music.getSongTitle());
-                            music.stopClip();
-                            thread.stop();
+                            //System.out.println(music.getSongTitle());
+                            music.get(music.size()-1).stopClip();
+                            //thread.stop();
                             System.out.println("Thread stopped: " + thread + thread.isAlive());
+                        }
+                    }*/
+                    for(int j = 0; j < music.size(); j++){
+                        System.out.println( music.get(j).getName() + "     " + jTop.getSongsList().get(i).getTitleSong().getText());
+                        if(music.get(j).getName().equals(jTop.getSongsList().get(i).getTitleSong().getText())){
+                            music.get(j).stopClip();
                         }
                     }
                 }

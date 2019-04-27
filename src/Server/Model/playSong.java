e14ee706df0e12f38a8a363d58c79e7bc0b8b593
Jiahu <Jiahui1@hotmail.es>
2019-04-27 14:54:18 +0100

    #25 Feat: Stops Music It stops the song if we click the Stop button

--- src/Server/Model/playSong.java
+++ src/Server/Model/playSong.java
@@ -4,37 +4,36 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
-public class playSong implements Runnable {
-    Clip clip;
+public class playSong extends Thread {
+    private Clip clip;
     private String songTitle;
 
     public playSong(String songTitle){
         this.songTitle = songTitle;
+        this.setName(songTitle);
     }
-
-    @Override
-    public void run() {
+    public void run(){
         AudioInputStream audioInputStream;
         try {
-            audioInputStream = AudioSystem.getAudioInputStream(new File("D:\\" + songTitle + ".wav").getAbsoluteFile());
+            audioInputStream = AudioSystem.getAudioInputStream(new File("songTemp/" + songTitle + ".wav").getAbsoluteFile());
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             //clip.loop(Clip.LOOP_CONTINUOUSLY);
             clip.start();
 
-            if(!Thread.currentThread().isAlive()){
+            /*if (!Thread.currentThread().isAlive()) {
                 clip.stop();
-            }
+            }*/
         } catch (UnsupportedAudioFileException | LineUnavailableException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     public void stopClip() {
          clip.stop();
          clip.close();
+         this.stop();
     }
 
     public void setClip(Clip clip) {

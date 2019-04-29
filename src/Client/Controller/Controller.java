f421acea4b6f4d8b4dc6c47f8eb9381a6ce7b36b
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-29 10:31:48 +0200

    #25 fix: Network established

--- src/Client/Controller/Controller.java
+++ src/Client/Controller/Controller.java
@@ -27,6 +27,7 @@ public class Controller {
 
     public void registerNetwork(ClientConnection network) {
         this.network = network;
+        network.establishConnection();
     }
 
     public void registerController() {

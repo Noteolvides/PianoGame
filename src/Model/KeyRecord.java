d6eca3fada64f18c9bd72df3c5d0529161d3f8d8
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-08 11:50:02 +0200

    #24 : Feat : id and getters and setters

--- src/Model/KeyRecord.java
+++ src/Model/KeyRecord.java
@@ -1,16 +1,38 @@
 package Model;
 
 public class KeyRecord {
+    private int id;
     private String key;
     private long start;
     private long end;
 
-    public KeyRecord(String str, long currentTimeMillis) {
+    public KeyRecord(String str, long currentTimeMillis,int id) {
+        this.id = id;
         this.key = str;
         this.start = currentTimeMillis;
     }
 
     public void setEnd(long end) {
         this.end = end;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

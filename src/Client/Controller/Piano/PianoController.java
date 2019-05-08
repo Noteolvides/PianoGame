a832adb1ae60399a8ed656c6c3e9f6b8ea4d48e5
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-08 10:30:45 +0200

    #24 Feat : Added recordSong button listener

--- src/Client/Controller/Piano/PianoController.java
+++ src/Client/Controller/Piano/PianoController.java
@@ -20,14 +20,9 @@ public class PianoController {
     private int actualOctave; //TODO : Revision of this atribute
     private RealtimePlayer realtimePlayer;
     private char[] keyBoardConfiguration = new char[]{'A', 'S', 'D', 'F', 'G', 'H', 'J', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'L'};//TODO : OTRO ATRIBUTO QUE DEBERIA ESTAR EN MODEL
-    private boolean[] isPresed = new boolean[24];
     private static int numberOFkeys = 0;
-    private HashMap<Integer, KeyRecord> keys = new HashMap<>();
-
-    public static void main(String[] args) {
-        //View view = new View();
-        //PianoController pianoController = new PianoController(view, controller);
-    }
+    private HashMap<Integer,KeyRecord> keys = new HashMap<>();
+    private boolean recordSong;
 
     public PianoController(View view, Controller controller) {
         this.view = view;
@@ -37,7 +32,9 @@ public class PianoController {
 
     private void initController() {
         view.initPianoView();
-        for (int i = 0; i < 6; i++) {
+
+        //Implementation of listener for keys 1234560 to change Octave;
+        for (int i = 0; i < 7; i++) {
             view.getPianoView().getPiano().getIm().put(KeyStroke.getKeyStroke(i+""), i+"");
             int finalI = i;
             view.getPianoView().getPiano().getAm().put(i+"", new AbstractAction() {
@@ -47,11 +44,16 @@ public class PianoController {
                 }
             });
         }
-        //TODO : No funcionan los botonoes
+        //TODO : No funcionan los botonoes creo que lo voy a cambiar
+
+
+        //To get out
         view.getPianoView().getTopOption().getExitToMenu().addActionListener(e -> {
             controller.closePiano();
             controller.openPrincipal();
         });
+
+        //To go open a song
         view.getPianoView().getTopOption().getSelectSongInSystem().addActionListener(e -> {
             controller.closePiano();
             controller.openSong();
@@ -59,12 +61,16 @@ public class PianoController {
 
         try {
             realtimePlayer = new RealtimePlayer();
+            controlKeys();
+            //Listener to record Song
+            view.getPianoView().getTopOption().getRecord().addActionListener(e ->{
+                recordSong = true;
+                view.getPianoView().getTopOption().getRecord().setEnabled(false);
+                keys.clear();
+            });
         } catch (MidiUnavailableException e) {
             e.printStackTrace();
         }
-
-        controlKeys();
-
     }
 
     private void controlKeys() {
@@ -80,10 +86,13 @@ public class PianoController {
                     if (activado[finalI] == 0) {
                         k.touch();
                         String str = k.getNumberOfKey().getText();
-                        int actualValue = Integer.valueOf(str.substring(str.length() -1 , str.length())) + 3;
+                        int actualValue = Integer.valueOf(str.substring(str.length() -1 , str.length())) + 2;
                         str = str.substring(0, str.length() - 1) + actualValue;
                         realtimePlayer.startNote(new Note(str));
-                        activado[finalI] = numberOFkeys++;
+                        if (recordSong){
+                            activado[finalI] = numberOFkeys+1;
+                            numberOFkeys++;
+                        }
                         keys.put(numberOFkeys,new KeyRecord(str,System.currentTimeMillis()));
                     }
                 }
@@ -95,7 +104,7 @@ public class PianoController {
                     activado[finalI] = 0;
                     k.unTouch();
                     String str = k.getNumberOfKey().getText();
-                    int actualValue = Integer.valueOf(str.substring(str.length() -1 , str.length())) + 3;
+                    int actualValue = Integer.valueOf(str.substring(str.length() -1 , str.length())) + 2;
                     str = str.substring(0, str.length() - 1) + actualValue;
                     realtimePlayer.stopNote(new Note(str));
                 }

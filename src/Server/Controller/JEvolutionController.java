d74d07553623ff39d82c72d31a549616f4b65e0e
Jiahu <Jiahui1@hotmail.es>
2019-05-12 11:12:57 +0200

    #25 Feat: Graphic Change Each button retrieves the data and shows the data correctly

--- src/Server/Controller/JEvolutionController.java
+++ src/Server/Controller/JEvolutionController.java
@@ -1,5 +1,6 @@
 package Server.Controller;
 
+import Server.Controller.BBDD.ServiceBBDD.ServiceBBDDServer;
 import Server.View.JEvolution;
 
 import javax.swing.*;
@@ -8,9 +9,11 @@ import java.awt.event.ActionListener;
 
 public class JEvolutionController implements ActionListener {
     private JEvolution jEvolution;
+    private ServiceBBDDServer service;
 
     public JEvolutionController(JEvolution jEvolution){
         this.jEvolution = jEvolution;
+        this.service = jEvolution.getService();
     }
 
     public void actionPerformed(ActionEvent event){
@@ -19,13 +22,22 @@ public class JEvolutionController implements ActionListener {
         switch (whichButton){
             case "Week":
                 //TODO: xDivions = 7
+                jEvolution.setUsers(service.getLastWeekConnections());
+                jEvolution.setGraphic(jEvolution.getUsers());
+                jEvolution.setEvoVisible(true);
                 System.out.println("Monday");
                 break;
             case "Month":
                 //TODO: xDivions = 12
+                jEvolution.setUsers(service.getLastMonthConnections());
+                jEvolution.setGraphic(jEvolution.getUsers());
+                jEvolution.setEvoVisible(true);
                 System.out.println("January");
                 break;
             case "Year":
+                jEvolution.setUsers(service.getLastYearConnections());
+                jEvolution.setGraphic(jEvolution.getUsers());
+                jEvolution.setEvoVisible(true);
                 System.out.println("2019");
                 break;
         }

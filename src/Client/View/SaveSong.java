783530c339e6146df1d4b11daa5c5f67e306757d
Jiahu <Jiahui1@hotmail.es>
2019-05-15 11:57:39 +0200

    #28 Feat: Adavancing SaveSong

--- src/Client/View/SaveSong.java
+++ src/Client/View/SaveSong.java
@@ -10,17 +10,48 @@ public class SaveSong extends JFrame{
     private JButton okButton;
 
     public SaveSong() {
+        wannaPrivate = new JCheckBox("Enable Song Privacy");
+        okButton = new JButton("Save Song");
+        addSongName = new JTextField();
 
+        setLayout(new GridLayout(3,1));
+        add(addSongName);
+        add(wannaPrivate);
+        add(okButton);
 
         setSize(250, 300);
         setTitle("Register");
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        //setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setResizable(false);
-        setVisible(false);
+        setVisible(true);
     }
 
     public void registerController(ActionListener c) {
 
+    }
+
+    public JCheckBox getWannaPrivate() {
+        return wannaPrivate;
+    }
+
+    public void setWannaPrivate(JCheckBox wannaPrivate) {
+        this.wannaPrivate = wannaPrivate;
+    }
+
+    public JTextField getAddSongName() {
+        return addSongName;
+    }
+
+    public void setAddSongName(JTextField addSongName) {
+        this.addSongName = addSongName;
+    }
+
+    public JButton getOkButton() {
+        return okButton;
+    }
+
+    public void setOkButton(JButton okButton) {
+        this.okButton = okButton;
     }
 }

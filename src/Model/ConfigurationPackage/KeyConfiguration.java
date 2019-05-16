9cfa712804588d5342762d2a793834c1f706dff0
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-16 17:28:25 +0200

    #25 Feat : Added KeyMaping to Configuration

--- src/Model/ConfigurationPackage/KeyConfiguration.java
+++ src/Model/ConfigurationPackage/KeyConfiguration.java
@@ -3,4 +3,9 @@ package Model.ConfigurationPackage;
 public class KeyConfiguration {
     private String name;
     char key;
+
+    public KeyConfiguration(String nameKey, char i) {
+        this.name = nameKey;
+        key = i;
+    }
 }

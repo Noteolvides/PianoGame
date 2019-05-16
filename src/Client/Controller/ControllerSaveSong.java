15935ca5437c34f76c5f0185870483d02b06faf9
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-16 19:00:39 +0200

    #28 feat: Added SaveSong controller

---
+++ src/Client/Controller/ControllerSaveSong.java
@@ -0,0 +1,24 @@
+package Client.Controller;
+
+import Client.View.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControllerSaveSong implements ActionListener {
+    private View view;
+    private Controller controller;
+
+    public ControllerSaveSong(View view, Controller controller) {
+        this.view = view;
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("SAVE-SONG")){
+            controller.setSongToSave(view.getSaveSongView().getAddSongName().getAccessibleContext().toString());
+            controller.networkSaveSong();
+        }
+    }
+}

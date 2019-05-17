7cc1792a930a57a9b3c0950b047c5fecf21c9474
Jiahu <Jiahui1@hotmail.es>
2019-05-17 15:01:25 +0200

    #28 Feat: SaveSong View Done

--- src/Client/View/SaveSong.java
+++ src/Client/View/SaveSong.java
@@ -1,6 +1,7 @@
 package Client.View;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
@@ -9,27 +10,41 @@ public class SaveSong extends JFrame{
     private JTextField addSongName;
     private JButton okButton;
     private JTextArea songDescription;
+    private JLabel songName;
+    private JLabel songDesc;
+
     public SaveSong() {
         wannaPrivate = new JCheckBox("Enable Song Privacy");
         okButton = new JButton("Save Song");
         addSongName = new JTextField();
         songDescription = new JTextArea();
+        songDescription.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+        songDesc = new JLabel("Add Description");
+        songName = new JLabel("Add Song Title");
+
 
         setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 0;
         constraints.gridy = 0;
+        add(songName, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
         constraints.weightx = 1;
         add(addSongName, constraints);
-        constraints.gridy = 1;
+        constraints.gridy = 2;
+        add(songDesc, constraints);
+        constraints.gridy = 3;
         constraints.ipady = 50;
         add(songDescription, constraints);
         constraints.weightx = 0;
-        constraints.gridy = 2;
+        constraints.gridy = 4;
         constraints.ipady = 0;
         add(wannaPrivate, constraints);
-        constraints.gridy = 3;
+        constraints.fill = 0;
+        constraints.gridy = 5;
         add(okButton, constraints);
 
         setSize(400, 300);
@@ -37,9 +52,12 @@ public class SaveSong extends JFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setResizable(false);
-        setVisible(false);
+        setVisible(true);
     }
 
+    public static void main(String[] args) {
+        SaveSong saveSong = new SaveSong();
+    }
     public void registerController(ActionListener c) {
         okButton.setActionCommand("SAVE-SONG");
         okButton.addActionListener(c);

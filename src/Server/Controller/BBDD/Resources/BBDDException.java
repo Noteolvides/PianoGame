aa1bd19a6b8db755b16ee8b76262efd6467d86df
joseproig <josep.roig@students.salle.url.edu>
2019-05-18 03:49:11 +0200

    #21 doc: Commented the BBDDException class

--- src/Server/Controller/BBDD/Resources/BBDDException.java
+++ src/Server/Controller/BBDD/Resources/BBDDException.java
@@ -1,8 +1,32 @@
 package Server.Controller.BBDD.Resources;
 
+/**
+ * This is a class that will take care of informing the user of any type of error
+ * that could generate errors in the database. That is, it is a class that guarantees
+ * the reliability of all queries.
+ */
 public class BBDDException extends Exception {
+    /**
+     * Basic constructor to generate the exception (without an own message, and the cause of the excpetion)
+     */
     public BBDDException() { super(); }
+
+    /**
+     * It's the same method than the basic constructor but it allows us to pass an string to show the error
+     * @param message Error message that we want to advert to the user of our program
+     */
     public BBDDException(String message) { super(message); }
+
+    /**
+     * It's a constructor that we can use in case that we want to show the cause and the message when there is a BBDDECEPTION
+     * @param message Error message that we want to advert to the user of our program
+     * @param cause The cause that have generated the database exception
+     */
     public BBDDException(String message, Throwable cause) { super(message, cause); }
+
+    /**
+     * It's a constructor that we can use in case that we want to show only the cause of the exception, without a own error message
+     * @param cause The cause that have generated the database exception
+     */
     public BBDDException(Throwable cause) { super(cause); }
 }

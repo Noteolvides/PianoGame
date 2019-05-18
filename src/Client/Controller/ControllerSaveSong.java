66feb8c28baf587cdad20df08c9d531f1f4db81f
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-19 00:21:22 +0200

    #27 doc: Commented SaveSong controller

--- src/Client/Controller/ControllerSaveSong.java
+++ src/Client/Controller/ControllerSaveSong.java
@@ -5,16 +5,29 @@ import Client.View.View;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Controller class for the SaveSong view
+ * that implements ActionListener.
+ */
 public class ControllerSaveSong implements ActionListener {
     private View view;
     private Controller controller;
 
+    /**
+     * SaveSong Controller constructor, it assigns the view and the father controller
+     * @param view
+     * @param controller
+     */
     public ControllerSaveSong(View view, Controller controller) {
         this.view = view;
         this.controller = controller;
 
     }
 
+    /**
+     * Method implemented from the ActionListener.
+     * @param e Action done in the View.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("SAVE-SONG")){

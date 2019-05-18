4ef1865eb0afd9996575f122853b24171ed3d9a6
joseproig <josep.roig@students.salle.url.edu>
2019-05-18 03:07:01 +0200

    #21 doc: Commented the enumeration class Avaiable client

--- src/Server/Controller/BBDD/MultiConnection/AvaiableClients.java
+++ src/Server/Controller/BBDD/MultiConnection/AvaiableClients.java
@@ -1,6 +1,8 @@
 package Server.Controller.BBDD.MultiConnection;
 
-//This class helps us to identify the different users
+/**
+ * This enumeration helps us to identify the different users, so it's just a class that helps the visibility of the code
+ */
 public enum AvaiableClients {
     adminSmartPiano, noUserSmartPiano , UserRegistered
 }

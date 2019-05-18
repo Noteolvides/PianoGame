1c19932330f9d7e8d2d302d8ea842710075d5eb8
joseproig <josep.roig@students.salle.url.edu>
2019-05-18 03:15:46 +0200

    #21 doc: Commented the Abstract routing class

--- src/Server/Controller/BBDD/MultiConnection/AbstractRouting.java
+++ src/Server/Controller/BBDD/MultiConnection/AbstractRouting.java
@@ -3,11 +3,14 @@ package Server.Controller.BBDD.MultiConnection;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 
-// This is the method that deals with controlling connections (knows which datasource to use at any moment ...). For example,
-// I'm pretty sure that when you change the connection specified in the ContextHolder class, it takes care to
-// establish the new connection
+/** This is the method that deals with controlling connections (knows which datasource to use at any moment ...).
+ * For example, when you change the connection specified in the ContextHolder class, it takes care to establish
+ * the new connection **/
 public class AbstractRouting extends AbstractRoutingDataSource {
-
+    /**
+     * Call the appropriate database based on the actual current key,
+     * @return Then this class returns the different connections related with a datasource
+     */
     @Override
     protected Object determineCurrentLookupKey() {
         return ServerContextHolder.getAvaiableClients();

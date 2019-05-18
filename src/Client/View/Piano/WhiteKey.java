4c2a5ea4134be18711d1fd98fdaf2331514cc3d5
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-19 00:21:50 +0200

    #21 Doc : WhiteKey

--- src/Client/View/Piano/WhiteKey.java
+++ src/Client/View/Piano/WhiteKey.java
@@ -3,8 +3,26 @@ package Client.View.Piano;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
 
+
+/**
+ * Whithe is a extends of a Key to show a blackKey in the pianoView
+ *
+ * @author  Gerard
+ * @author Gustavo
+ * @author Neil
+ * @author Jiahui
+ * @author Josep
+ * @version 1.0
+ * @since 2019-05-16
+ */
 public class WhiteKey extends Key {
 
+
+    /**
+     * Constructor of WhitheKey
+     * @param i        The number in that octave
+     * @param levelKey The octave that this key represents
+     */
     public WhiteKey(int i, int levelKey) {
         int iCopy = i;
         if (i >= 8){

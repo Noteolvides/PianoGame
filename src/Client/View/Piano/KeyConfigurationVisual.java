da80dd90078f726f16cc4cf18b559dd74655f9ae
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-19 00:05:40 +0200

    #21 Doc Key Configuration

--- src/Client/View/Piano/KeyConfigurationVisual.java
+++ src/Client/View/Piano/KeyConfigurationVisual.java
@@ -5,10 +5,26 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/**
+ * Component in view ChangeKeys, that contais a textField and name
+ *
+ * @author  Gerard
+ * @author Gustavo
+ * @author Neil
+ * @author Jiahui
+ * @author Josep
+ * @version 1.0
+ * @since 2019-05-16
+ */
 public class KeyConfigurationVisual extends JPanel {
     private JLabel name;
     private JTextField key;
 
+    /**
+     * Initialization of keyConfiguration
+     * @param name Name of key to change
+     * @param keyArg Key that it is going to be change to
+     */
     public KeyConfigurationVisual(String name, char keyArg) {
         setLayout(new FlowLayout());
         this.name = new JLabel(name);

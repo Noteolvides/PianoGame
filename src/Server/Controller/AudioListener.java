d396be6fe68e9f3e008a2dd929e48e091c5647a3
Jiahu <Jiahui1@hotmail.es>
2019-05-19 18:14:36 +0200

    #27 Feat: AudioListener Javadoc

--- src/Server/Controller/AudioListener.java
+++ src/Server/Controller/AudioListener.java
@@ -4,9 +4,29 @@ package Server.Controller;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 
+/**
+ * Class that complements with playSong Class
+ * It determines whether a song should be played
+ * or stopped
+ *
+ * @version 1.0
+ * @since 2019-05-19
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
+ */
 public class AudioListener implements LineListener {
     private boolean done = false;
 
+    /**
+     * Determines the line of the song that is currently playing
+     * After the last line it stops the song
+     * @param event: Line fo the song
+     */
     @Override
     public synchronized void update(LineEvent event) {
         LineEvent.Type eventType = event.getType();
@@ -15,6 +35,11 @@ public class AudioListener implements LineListener {
             notifyAll();
         }
     }
+
+    /**
+     * Waits until the song is finished
+     * @throws InterruptedException
+     */
     public synchronized void waitUntilDone() throws InterruptedException {
         while (!done) { wait(); }
     }

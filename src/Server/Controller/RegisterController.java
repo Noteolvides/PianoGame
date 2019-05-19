15e291f68c9b1b8047fd6c9021b63c315c7c40b9
Jiahu <Jiahui1@hotmail.es>
2019-05-19 18:14:53 +0200

    #27 Feat: RegisterController Javadoc

--- src/Server/Controller/RegisterController.java
+++ src/Server/Controller/RegisterController.java
@@ -7,6 +7,21 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Controller for the Register Class
+ * Determines the actions to be done after
+ * clicking the register button
+ *
+ * @version 1.0
+ * @since 2019-05-19
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
+ */
 public class RegisterController implements ActionListener {
     private View view;
     private ServiceBBDDServer serviceBBDDServer;

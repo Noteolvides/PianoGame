9d10dbf1043f88f6b5bc920491b092d11cf3df90
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-19 15:16:46 +0200

    #25 Doc : Key Configuration

--- src/Model/ConfigurationPackage/KeyConfiguration.java
+++ src/Model/ConfigurationPackage/KeyConfiguration.java
@@ -1,9 +1,27 @@
 package Model.ConfigurationPackage;
 
+/**
+ * This class contains the key Configuration of one key
+ *
+ * @version 1.0
+ * @since 2019-05-16
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
+ */
 public class KeyConfiguration {
     private String name;
     char key;
 
+    /**
+     *
+     * @param nameKey The name that this is going to be like firstOctave
+     * @param i The key that it is going to be
+     */
     public KeyConfiguration(String nameKey, char i) {
         this.name = nameKey;
         key = i;

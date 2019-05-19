645cfc63624adce2e38bf54cccebe391c8cd363f
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-19 15:17:12 +0200

    #25 Doc : KeyRecorder

--- src/Model/KeyRecord.java
+++ src/Model/KeyRecord.java
@@ -1,11 +1,30 @@
 package Model;
 
+/**
+ * This class contains the note information of a note that was presed and how long it was
+ *
+ * @version 1.0
+ * @since 2019-05-16
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
+ */
 public class KeyRecord {
     private int id;
     private String key;
     private long start;
     private long end;
 
+    /**
+     *
+     * @param str The note
+     * @param currentTimeMillis StartTime
+     * @param id Identifier
+     */
     public KeyRecord(String str, long currentTimeMillis,int id) {
         this.id = id;
         this.key = str;

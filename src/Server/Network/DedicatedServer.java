64e4b4e14e163f1c84173754173d57deeb3c17d7
joseproig <josep.roig@students.salle.url.edu>
2019-05-19 12:50:30 +0200

    #25 fix: Solved the error when reproduce a system song, and charged the system songs

--- src/Server/Network/DedicatedServer.java
+++ src/Server/Network/DedicatedServer.java
@@ -276,11 +276,11 @@ public class DedicatedServer extends Thread {
 
                         //I return the song, so i can get the path i then i can get the song and pass it
                         Song songObtained = null;
-                        try {
-                            songObtained = service.getConcreteSongUser(author, song);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+
+
+                        songObtained = service.getConcreteSongUser(userSave,author, song);
+
+
 
                         songObtained.setPlays(songObtained.getPlays() + 1);
                         service.updateSong(songObtained);

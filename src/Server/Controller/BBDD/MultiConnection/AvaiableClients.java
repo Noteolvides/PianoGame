ff3f5f8d8b0871d2af71f95fa0fbb0139d37ae08
Melgui <gmelgares9@gmail.com>
2019-05-19 13:32:24 +0200

    #27 doc: Commented AvaiableClients Class

--- src/Server/Controller/BBDD/MultiConnection/AvaiableClients.java
+++ src/Server/Controller/BBDD/MultiConnection/AvaiableClients.java
@@ -2,6 +2,17 @@ package Server.Controller.BBDD.MultiConnection;
 
 /**
  * This enumeration helps us to identify the different users, so it's just a class that helps the visibility of the code
+ *
+ *
+ * @version 1.0
+ * @since 2019-05-19
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
  */
 public enum AvaiableClients {
     adminSmartPiano, noUserSmartPiano , UserRegistered

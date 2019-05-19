2df732e277484ab0e894925d53986f12caef3fe9
Melgui <gmelgares9@gmail.com>
2019-05-19 13:22:21 +0200

    #27 doc: Commented TopPanel Class

--- src/Client/View/Piano/TopPanel.java
+++ src/Client/View/Piano/TopPanel.java
@@ -3,17 +3,18 @@ package Client.View.Piano;
 import javax.swing.*;
 import java.awt.*;
 
-
 /**
  * Auxiliar Component to JPiano that contains all submenus in the view
  *
- * @author  Gerard
- * @author Gustavo
- * @author Neil
- * @author Jiahui
- * @author Josep
  * @version 1.0
- * @since 2019-05-16
+ * @since 2019-05-19
+ *
+ * @author Gustavo Gómez
+ * @author Gerard Melgares
+ * @author Josep Roig
+ * @author Neil Torrero
+ * @author Jiahui Xie
+ *
  */
 public class TopPanel extends JMenuBar {
     private JMenu recordSong;
